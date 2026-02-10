036b66da70a8b914f4f0725bfb9e48fe90828f26
Christian Sanches <christian.sanches-v@uplevelteam.com>
2026-02-10 14:36:00 -0300

    removed .* from imports

--- backend/src/test/java/org/desafio/backend/service/PautaServiceTest.java
+++ backend/src/test/java/org/desafio/backend/service/PautaServiceTest.java
@@ -11,7 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
