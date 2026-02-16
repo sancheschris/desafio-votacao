cdd6c8e84563ac43a2bf48e8c12464e3caa56047
Christian Sanches <christian.sanches-v@uplevelteam.com>
2026-02-16 18:28:53 -0300

    adds api versioning

--- backend/src/main/java/org/desafio/backend/controller/PautaController.java
+++ backend/src/main/java/org/desafio/backend/controller/PautaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pautas")
+@RequestMapping("/api/v1/pautas")
 public class PautaController {
     private final PautaService pautaService;
 
